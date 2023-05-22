f317634bfd3ba485b50b9e823390b57b58cf3987
BastianLo <bastian.lobe@t-online.de>
2023-05-22 21:19:23 +0200

    Nutrient filter recipe rest api

--- src/Apps/MealManager/filters.py
+++ src/Apps/MealManager/filters.py
@@ -1,9 +1,47 @@
 from dj_rql.filter_cls import RQLFilterClass, AutoRQLFilterClass
+from dj_rql.qs import NSR
 
 from .models import Recipe, Cuisine
 
 class RecipeFilters(AutoRQLFilterClass):
     MODEL = Recipe
+
+    FILTERS = [
+        {
+            'filter': 'protein',
+            'source': 'nutrients__protein',
+        },
+        {
+            'filter': 'kj',
+            'source': 'nutrients__energyKj',
+        },
+        {
+            'filter': 'kcal',
+            'source': 'nutrients__energyKcal',
+        },
+        {
+            'filter': 'fat',
+            'source': 'nutrients__fat',
+        },
+        {
+            'filter': 'fatSat',
+            'source': 'nutrients__fatSaturated',
+        },
+        {
+            'filter': 'carbs',
+            'source': 'nutrients__carbs',
+        },
+        {
+            'filter': 'sugar',
+            'source': 'nutrients__sugar',
+        },
+        {
+            'filter': 'salt',
+            'source': 'nutrients__salt',
+        },
+    ]
+    class Meta:
+        fields = '__all__'
 
 class CuisineFilters(AutoRQLFilterClass):
     MODEL = Cuisine
