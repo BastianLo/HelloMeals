cdcfe3a122d17e1c7bdd890df5fab0dfb2254f10
BastianLo <bastian.lobe@t-online.de>
2023-05-24 16:18:24 +0200

    update site

--- src/HelloMeals/settings.py
+++ src/HelloMeals/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'rql_filter',
     'rest_framework',
     'django_filters',
+    'pwa',
 
     'Apps.MealManager',
     'Apps.ClientManager',
@@ -216,3 +217,34 @@ LOGGING = {
         'level': 'DEBUG',
     }
 }
+
+
+PWA_APP_NAME = 'HelloMeals'
+PWA_APP_DESCRIPTION = "Selfhosted HelloFresh recipe manager"
+PWA_APP_THEME_COLOR = '#0F172A'
+PWA_APP_BACKGROUND_COLOR = '#0F172A'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/Home'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/images/logo/512px.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/logo/512px.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/icons/splash-640x1136.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
--- src/util/pagination.py
+++ src/util/pagination.py
@@ -1,6 +1,19 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 class RqlPagination(PageNumberPagination):
     page_size = 10  # Number of items per page
     max_page_size = 1000  # Maximum number of items per page
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        start = (self.page.number - 1) * self.page.paginator.per_page + 1
+        end = start + len(data) - 1
+
+        return Response({
+            'count': self.page.paginator.count,
+            'start': start,
+            'end': end,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
