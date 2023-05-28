d4d7db5470d9275d8656bc807366497dcee2ff0e
BastianLo <bastian.lobe@t-online.de>
2023-05-28 17:07:06 +0200

    Back button & Settings menu

--- src/Apps/ClientManager/views.py
+++ src/Apps/ClientManager/views.py
@@ -9,34 +9,17 @@ def get_base_context():
 
 
 def navigation_history(request):
-    # Retrieve the navigation history list from the session
-    ng = request.session.get('navigation_history', [])
-    # Append the current URL to the history list
-    ng.append(request.META.get('HTTP_REFERER'))
-    # Limit the history list to a maximum number of URLs if desired
-    max_history_length = 10  # Set your desired maximum history length
-    ng = ng[-max_history_length:]
-    # Update the session with the updated history list
-    request.session['navigation_history'] = ng
-    # Your view logic here
-
+    request.session['previous_url'] = request.META.get('HTTP_REFERER')
 
 def back_view(request):
-    # Retrieve the navigation history list from the session
-    ng = request.session.get('navigation_history', [])
-    if len(ng) > 1:
-        # Remove the current URL from the history list
-        ng.pop()
-        # Get the previous URL from the updated history list
-        previous_url = ng[-1]
-        # Update the session with the updated history list
-        request.session['navigation_history'] = ng
+    # Retrieve the previous URL from the session
+    previous_url = request.session.get('previous_url')
+    if previous_url:
         # Redirect the user to the previous URL
         return redirect(previous_url)
     else:
-        # Handle the case when there are no previous URLs available
+        # Handle the case when there is no previous URL available
         return HttpResponse("Previous URL not found.")
-
 
 @login_required(login_url='/accounts/login/')
 def index(request):
