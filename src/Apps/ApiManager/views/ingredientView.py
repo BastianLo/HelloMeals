33ac92b86a66082da74f26f37285a7ea4d2abc88
BastianLo <bastian.lobe@t-online.de>
2023-06-19 20:46:00 +0200

    increase page size for stock

--- src/Apps/ApiManager/views/ingredientView.py
+++ src/Apps/ApiManager/views/ingredientView.py
@@ -50,6 +50,7 @@ class IngredientList(generics.ListAPIView):
 @permission_classes([IsAuthenticated])
 class stockList(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = RqlPagination
     filterset_class = IngredientFilterSet
 
     def get_serializer_class(self):
@@ -82,6 +83,7 @@ def add_ingredient_to_stock(request, ingredient_id):
 @permission_classes([IsAuthenticated])
 class shoppingListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = RqlPagination
     filterset_class = IngredientFilterSet
 
     def get_serializer_class(self):
