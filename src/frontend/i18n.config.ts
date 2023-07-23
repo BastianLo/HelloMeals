ada5873e49b3f79ab22741a563aa2fec13fef7b3
BastianLo <bastian.lobe@t-online.de>
2023-07-23 22:52:21 +0200

    adjust locale

--- src/frontend/i18n.config.ts
+++ src/frontend/i18n.config.ts
@@ -3,10 +3,34 @@ export default defineI18nConfig(() => ({
     locale: 'en',
     messages: {
         en: {
-            welcome: 'Welcome'
+            welcome: 'Welcome',
+            recipe: {
+                navLabel: 'Recipes'
+            },
+            storage: {
+                navLabel: 'Storage'
+            },
+            planner: {
+                navLabel: 'Planner'
+            },
+            settings: {
+                navLabel: 'Settings'
+            },
         },
         de: {
-            welcome: 'Willkommen'
+            welcome: 'Willkommen',
+            recipe: {
+                navLabel: 'Rezepte'
+            },
+            storage: {
+                navLabel: 'Vorrat'
+            },
+            planner: {
+                navLabel: 'Planer'
+            },
+            settings: {
+                navLabel: 'Einstellungen'
+            },
         }
     }
 }))
