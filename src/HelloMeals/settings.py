4d15a8a3f6a37182b7b60367911ad5e9756a8134
BastianLo <bastian.lobe@t-online.de>
2023-07-26 19:46:39 +0200

    increase JWT token lifetimes in settings
    Increased the lifetimes of both access and refresh JWT tokens in the settings file. This change was necessary to improve the user experience by reducing the frequency of logins, providing a longer term authentication solution.Adjust JWT token lifetimes in HelloMeals
    
    Extended the lifetimes of both access and refresh JWT tokens from seconds/minutes to days in the settings.py of HelloMeals. This change aims to enhance the user experience by decreasing the need for frequent logins, thereby offering a more efficient and long-term authentication approach.

--- src/HelloMeals/settings.py
+++ src/HelloMeals/settings.py
@@ -89,8 +89,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
 }
 
 ROOT_URLCONF = 'HelloMeals.urls'
