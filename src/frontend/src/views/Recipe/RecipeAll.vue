36f27e94d897f3b029ec7e4ba78cc512252030b5
BastianLo <bastian.lobe@t-online.de>
2023-07-30 12:21:39 +0200

    Create recipe card

--- src/frontend/src/views/Recipe/RecipeAll.vue
+++ src/frontend/src/views/Recipe/RecipeAll.vue
@@ -1,11 +1,16 @@
 <template>
   <p>abc</p>
   <h1>ABC</h1>
-  <p class="text-white">{{ recipeStore.recipes }}</p>
+  <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto mb-6 mt-10">
+    <RecipeCard v-for="recipe in recipeStore.recipes" :key="recipe" :item="recipe" :recipe="recipe">
+    </RecipeCard>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import {useRecipeStore} from "@/stores/RecipeStore";
+import RecipeCard from "@/components/Recipe/RecipeCard.vue";
 
 let recipeStore = useRecipeStore()
 recipeStore.fetch_recipes()
