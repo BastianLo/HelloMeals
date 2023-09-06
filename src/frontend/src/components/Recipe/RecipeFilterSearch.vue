e4391cc1278e2fdba84d465cfc21114b2db0876c
BastianLo <bastian.lobe@t-online.de>
2023-09-06 20:44:30 +0200

    fix filter modal width

--- src/frontend/src/components/Recipe/RecipeFilterSearch.vue
+++ src/frontend/src/components/Recipe/RecipeFilterSearch.vue
@@ -127,8 +127,8 @@ updateComponentValues()
     <div class="fixed inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 mb-40">
         <OnClickOutside @trigger="search()">
-          <div style="width: 400px"
-               class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div
+              class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-96 w-80">
             <div class="w-lg shadow p-5 rounded-lg bg-gray-800">
               <!-- Popup head -->
               <div class="flex items-center justify-between mt-4">
