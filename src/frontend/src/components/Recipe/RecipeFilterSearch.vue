984d0ca9ac43d66dfb90289de00cd15e44184549
BastianLo <bastian.lobe@t-online.de>
2023-09-06 20:22:47 +0200

    fix scrolling on filter modal

--- src/frontend/src/components/Recipe/RecipeFilterSearch.vue
+++ src/frontend/src/components/Recipe/RecipeFilterSearch.vue
@@ -122,11 +122,10 @@ updateComponentValues()
 
   <!-- Filter button and Searchbox -->
 
-  <div class="relative z-10 h-screen flex items-center justify-center" aria-labelledby="modal-title" role="dialog"
-       aria-modal="true" v-if="show">
+  <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" v-if="show">
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
     <div class="fixed inset-0 z-10 overflow-y-auto">
-      <div class="flex items-center justify-center h-full p-4 text-center sm:p-0">
+      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 mb-40">
         <OnClickOutside @trigger="search()">
           <div style="width: 400px"
                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
